3d1f358456e90e37067c20029df436f80cee61a9
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-07-24 06:28:57 +0530

    Day4 QN3 assignment

---
+++ Day4_QN3/CustomError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_QN3
+{
+    internal class CustomError : Exception
+    {
+        public CustomError(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Day4_QN3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Day4_QN3;
+
+namespace Day4_Assignment_Ques3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("=== Mini File Reader App ===");
+                Console.Write("Enter the full path of the file to read: ");
+                string filePath = Console.ReadLine();
+                if (!File.Exists(filePath))
+                {
+                    throw new CustomError("CustomError: File path not found!");
+                }
+
+                Console.WriteLine("\n--- File Contents ---\n");
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine(content);
+
+
+
+            }
+            catch (CustomError ce)
+            {
+                Console.WriteLine(ce.Message);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Access denied. You don't have permission to read this file.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("I/O Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+        }
+    }
+}
