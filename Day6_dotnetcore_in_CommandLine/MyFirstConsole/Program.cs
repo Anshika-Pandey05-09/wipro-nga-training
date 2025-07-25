afcdb399b708d180b173fdb145534c079238bd82
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-07-25 06:58:09 +0530

    Day6 CommandLine .NET core

---
+++ Day6_dotnetcore_in_CommandLine/MyFirstConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello from .NET Core!");
+        Console.WriteLine($"OS Description: {RuntimeInformation.OSDescription}");
+        Console.WriteLine($"OS Architecture: {RuntimeInformation.OSArchitecture}");
+        Console.WriteLine($"Process Architecture: {RuntimeInformation.ProcessArchitecture}");
+        Console.WriteLine($".NET Version: {RuntimeInformation.FrameworkDescription}");
+    }
+}
