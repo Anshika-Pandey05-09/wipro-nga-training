66eb16817572727bb6e62c79279e2b0821e24780
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-08-02 09:20:27 +0530

    Day3 Jagged array

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedarray = new int[3][];
+            jaggedarray[0] = new int[] { 1, 2, 3, 4 };
+            jaggedarray[1] = new int[] { 5, 6 };
+            jaggedarray[2] = new int[] { 7, 8, 9, 10, 11 };
+            foreach (var array in jaggedarray)
+            {
+                foreach (var item in array)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
