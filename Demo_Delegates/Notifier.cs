8435520483ea65a60614e20a59e5f2f673863a7e
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-08-04 09:01:58 +0530

    Day5 Delegates

---
+++ Demo_Delegates/Notifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Delegates
+{
+    public delegate void Notify(string message);
+    public class Notifier
+    {
+        public void SendEmail(string message)
+        {
+            Console.WriteLine($"Email sent with message: {message}");
+        }
+        public void SendSMS(string message)
+        {
+            Console.WriteLine($"SMS sent with message: {message}");
+        }
+    }
+}
